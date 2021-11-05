8178f4297900cc89df93dd823d944a9465c9545e
Christopher Mooney <cmooney@carhartt.com>
2021-11-05 10:18:03 -0500

    fixing bad test case

--- tests/test_simpleton.py
+++ tests/test_simpleton.py
@@ -3,5 +3,5 @@ from simpleton.process import chunks
 
 
 def test_chinks():
-   rslt = chunks(lst= [1,2,3,4,5,6],n = 2)
-   assert len(rslt) == 2
+   rslt = chunks(lst= [1,2,3,4,5,6],n = 3)
+   assert len(rslt) == 3
